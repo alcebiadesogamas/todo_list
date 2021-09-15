746cfb0cb24948537776d0b94752de082440980b
alcebiadesogamas <alcebiades.29@gmail.com>
2021-09-15 20:33:37 -0300

    Creating Respository and Project

---
+++ src/main/java/br/com/challenge/todoList/TodoListChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.todoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListChallengeApplication.class, args);
+	}
+
+}
