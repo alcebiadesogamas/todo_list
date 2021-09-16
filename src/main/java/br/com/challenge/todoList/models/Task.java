17c0106240dd423b2b0c4865df9f01f14a120480
alcebiadesogamas <alcebiades.29@gmail.com>
2021-09-15 21:28:54 -0300

    Creating test database and test profile

--- src/main/java/br/com/challenge/todoList/models/Task.java
+++ src/main/java/br/com/challenge/todoList/models/Task.java
@@ -4,10 +4,20 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Task implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+//	private Long id_toDoList;
 	private String description;
 	private Instant created_at;
 	private Instant updated_at;
